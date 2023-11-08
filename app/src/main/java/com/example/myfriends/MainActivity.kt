752761885a610706cea49dfb9e5818cef7ed12da
Orlenok1enchik <lena.elena.popova.2001@mail.ru>
2023-11-08 22:06:25 +0500

    Добавила код для подключения к API VK

--- app/src/main/java/com/example/myfriends/MainActivity.kt
+++ app/src/main/java/com/example/myfriends/MainActivity.kt
@@ -3,14 +3,51 @@ package com.example.myfriends
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.VKApiCallback
+import com.vk.api.sdk.VKTokenExpiredHandler
+import com.vk.api.sdk.auth.VKAuthenticationResult
+import com.vk.api.sdk.auth.VKScope
+import com.vk.api.sdk.utils.VKUtils
+import com.vk.dto.common.id.UserId
+import com.vk.sdk.api.friends.FriendsService
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val fingerprients = VKUtils.getCertificateFingerprint(this,this.getPackageName());
+        VK.initialize(this)
+//        val tokenTracker = object: VKTokenExpiredHandler {
+//            override fun onTokenExpired() {
+//                // token expired
+//            }
+//        }
 
+//        VK.addTokenExpiredHandler(tokenTracker)
+        val fingerprients = VKUtils.getCertificateFingerprint(this,this.packageName)
+//
+        val certificate = fingerprients?.joinToString()
+//
+        Log.d("TAG", "fingerprients: $certificate")
+
+        VK.login(this){
+            when(it){
+                is VKAuthenticationResult.Success -> {}
+                is VKAuthenticationResult.Failed -> {}
+            }
+        }.launch(listOf(VKScope.FRIENDS))
+
+        VK.execute(FriendsService().friendsGetAppUsers(), object: VKApiCallback<List<UserId>>{
+            override fun success(result: List<UserId>) {
+                Log.d("TAG", "success: ${result.joinToString()}")
+            }
+            override fun fail(error: Exception){
+                Log.e("TAG", error.toString())
+            }
+        })
     }
+
 }
 
 
